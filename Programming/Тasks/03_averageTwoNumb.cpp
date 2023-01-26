703a89efabe1f19d2c05c7424ba7e43aceecfd3b
tsvetelina03 <116883462+tsvetelina03@users.noreply.github.com>
2023-01-26 20:07:46 +0200

    Create 03_averageTwoNumb.cpp

---
+++ Programming/Тasks/03_averageTwoNumb.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+double a,b,c,avg;
+int main()
+{
+    cout<<"Vuvedete 2 realni chisla: ";
+    cin>>a>>b;
+    
+    avg=(a+b)/2.0;
+    
+    cout<<"Rezultata e: ";
+    cout<<avg;
+ 
+    return 0;
+}
