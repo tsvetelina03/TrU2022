b89a3913a347449625142c69db6c592e9ccac9da
tsvetelina03 <116883462+tsvetelina03@users.noreply.github.com>
2023-01-27 11:30:08 +0200

    Create 05_NumbToText.cpp

---
+++ Programming/Conditional Statements/05_NumbToText.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int x;
+ 
+int main() {
+    
+    cout<<"chislo ";
+    cin>>x;
+    
+    if(x==0)cout<<"nula";
+   
+    else if (x==1)cout<<"edno";
+    else if (x==2)cout<<"dve";
+    else if (x==3)cout<<"tri";
+    else if (x==4)cout<<"chetiri";
+    else if (x==5)cout<<"pet";
+    else if (x==6)cout<<"shest";
+    else if (x==7)cout<<"sedem";
+    else if (x==8)cout<<"osem";
+    else cout<<"devet";
+    
+ 
+ 
+   
+return 0;
+ 
+}
