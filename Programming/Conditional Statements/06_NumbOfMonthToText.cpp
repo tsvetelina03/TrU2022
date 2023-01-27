a78dd0cd8eba266ac682b41d24b87fecd31a4738
tsvetelina03 <116883462+tsvetelina03@users.noreply.github.com>
2023-01-27 11:31:17 +0200

    Create 06_NumbOfMonthToText.cpp

---
+++ Programming/Conditional Statements/06_NumbOfMonthToText.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int x;
+ 
+int main() {
+    
+    cout<<"chislo ";
+    cin>>x;
+    
+    switch(x){
+   case 1: cout<<"qnuari";break;
+   case 2: cout<<"fevruari";break;
+   case 3: cout<<"mart";break;
+   case 4: cout<<"april";break;
+   case 5: cout<<"mai";break;
+   case 6: cout<<"iuni";break;
+   case 7: cout<<"iuli";break;
+   case 8: cout<<"avgust";break;
+   case 9: cout<<"septemvri";break;
+   case 10: cout<<"octomvri";break;
+   case 11: cout<<"noemvri";break;
+   case 12: cout<<"decemvri";break;
+   default:cout<<"nevalidna stoinost";
+    }
+   
+  
+   
+return 0;
+ 
+}
