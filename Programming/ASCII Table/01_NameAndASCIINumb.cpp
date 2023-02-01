5984e05239ac268f99c035a70f6838cf505d92b0
tsvetelina03 <116883462+tsvetelina03@users.noreply.github.com>
2023-02-01 12:07:11 +0200

    Update and rename 01_NameAndASCIINumb to 01_NameAndASCIINumb.cpp

---
+++ Programming/ASCII Table/01_NameAndASCIINumb.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+  cout<<"T\t"<<(int)'T'<<"\n";
+  cout<<"S\t"<<(int)'S'<<"\n";
+  cout<<"V\t"<<(int)'V'<<"\n";
+  cout<<"E\t"<<(int)'E'<<"\n";
+  cout<<"T\t"<<(int)'T'<<"\n";
+  cout<<"E\t"<<(int)'E'<<"\n";
+  cout<<"L\t"<<(int)'L'<<"\n";
+  cout<<"I\t"<<(int)'I'<<"\n";
+  cout<<"N\t"<<(int)'N'<<"\n";
+  cout<<"A\t"<<(int)'A'<<"\n";
+ 
+    return 0;
+}
