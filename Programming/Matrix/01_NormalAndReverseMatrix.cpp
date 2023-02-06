35e90fa2bb5aa4b278750b7af54ea5097de1427a
tsvetelina03 <116883462+tsvetelina03@users.noreply.github.com>
2023-02-06 14:15:58 +0200

    Create 01_NormalAndReverseMatrix.cpp

---
+++ Programming/Matrix/01_NormalAndReverseMatrix.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+int red,kolona;
+double numbers[10][20];
+int main()
+{
+do {
+cout << "Insert the lenght of the column massive(integer beetween 1 and 10):";
+cin >> red;
+} while (red < 1 || kolona> 10);
+do {
+cout << "Insert the lenght of the row massive(integer beetween 1 and 20):";
+cin >> kolona;
+} while (kolona < 1 || kolona> 20);
+ 
+ 
+for (int i = 0; i < red; i++)
+{
+for (int j = 0; j < kolona;  j++)
+{
+cout << "A[" << i << "] [" << j << "]= ";
+cin >> numbers[i][j];
+}
+
+}
+cout << "------------ \n";
+ 
+//Normal Matrix
+for (int i = 0;  i < red;  i++)
+{
+for (int j = 0; j < kolona; j++)
+{
+cout << numbers[i][j] << "\t";
+}
+cout << "\n";
+}
+ 
+cout << "------------ \n";
+//Reverse Matrtix
+for (int i = red - 1; i >= 0; i--)
+{
+for (int j = kolona - 1; j >= 0; j--)
+{
+cout << numbers[i][j] << "\t";
+}
+cout << "\n";
+}
+return 0;
+ 
+ 
+}
